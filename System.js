7c278692ba2b77e42b7bc082dad637087ec3d407
kaycal <scvdermody@gmail.com>
2015-01-12 21:13:48 -0800

    OK technically it's not a galaxy

--- System.js
+++ System.js
@@ -1,54 +1,56 @@
-Galaxy = function(url) {
+System = function(url) {
+    /*
+        Set up the basics for the scene, including;
+            - Parent call
+            - Camera
+            - Renderer
+            - Clock
+    */
     THREE.Scene.call( this );
-    // Set default values and standards;
-    //  Camera
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 2300 ); // Holes appear just outside of skybubble
+
+    // Change this to fit out skybox; 2200 for 1000 skybubble seems good
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // Holes appear just outside of skybubble
         camera.position.set(300,300,150);
         camera.lookAt(new THREE.Vector3(0,0,0));
     this.camera = camera;
 
-    //  Renderer
     var renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
+    document.body.appendChild( renderer.domElement );
     this.renderer = renderer;
 
-    //  Clock & Controls
     this.clock = new THREE.Clock();
     this.controls = new THREE.TrackballControls(this.camera);
 
+    this.loadSystem(url);
+
     // Get vital stats
     //  Planets
-    
+}
+
+
+
+//
+// Class builder functions
+/////////////////////////////
+System.prototype = Object.create( THREE.Scene.prototype );
+
+System.prototype.loadSystem = function(url) {
     $.getJSON(url, function(data) {
         console.log(data);
     })
+    // Experimental crap follows;
 
-    // ambient light
-    this.add(new THREE.AmbientLight( 0x666699 ));
-
-    // Point light
     var pl = new THREE.PointLight( 0xffffff, 4, 1000 );
     pl.position.set( 0, 0, 0);
     this.add(pl); // set position.set( 0, 0, 0 );
 
-
-
     this.planets = [];
     p = new Planet(this.add.bind(this));
     this.planets.push(p);
-/*
-    for (i=0;i<this.planets.length;i++) {
-        this.add(this.planets[i]);
-    } */
-//    console.log(this.planets[0]);
 }
-//
-// Class builder functions
-/////////////////////////////
-Galaxy.prototype = Object.create( THREE.Scene.prototype );
 
-Galaxy.prototype.render = function() {
+System.prototype.render = function() {
     requestAnimationFrame ( this.render.bind(this) );
     this.controls.update()
     delta = this.clock.getDelta();
@@ -58,6 +60,6 @@ Galaxy.prototype.render = function() {
     this.renderer.render( this, this.camera );
 }
 
-Galaxy.prototype.playToggle = function(playBool, timeFrame) {
+System.prototype.playToggle = function(playBool, timeFrame) { // This should eventually switch out to an actual toggle
     this.render();
 }
